1ee16cba3702da3343f4d6ac46da8b4df3e4c143
shockingboom <fajar050408@gmail.com>
2025-03-27 15:47:22 +0700

    Add files via upload

---
+++ Pert4/index.js
@@ -0,0 +1,10 @@
+let nama = "Budi"; // String
+let umur = 30; // Number
+let isStudent = false; // Boolean
+let buah = ["Apel", "Mangga", "Jeruk"]; // Array
+let mahasiswa = { nama: "Rina", usia: 20, jurusan: "Informatika" }; // Object
+
+
+console.log(nama);  // Output: Budi
+console.log(buah[1]); // Output: Mangga
+console.log(mahasiswa.jurusan); // Output: Informatika
