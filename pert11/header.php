d70829447e37d5ae2aa2c426c7ef25cfe7a21190
shockingboom <fajar050408@gmail.com>
2025-06-19 15:50:58 +0700

    Add files via upload

---
+++ pert11/header.php
@@ -0,0 +1,7 @@
+<html>
+   <body>
+       <div class="menu">
+           <?php include 'menu.php'; ?>
+       </div>
+   </body>
+</html>
---
+++ pert11/index.php
@@ -0,0 +1,3 @@
+<?php
+echo '<a href="index.php">Beranda</a> | <a href="about.php">Tentang</a> | <a href="contact.php">Kontak</a>';
+?>
